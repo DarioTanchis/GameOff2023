6095bcbe3dcf87f5da503eabd0b3e940782226c6
Kallistotele <mikicabiddu@gmail.com>
2023-11-30 13:19:50 +0100

    200+ fix
    AHAHAHAH

--- Assets/Scripts/Michele/DoorInteractable.cs
+++ Assets/Scripts/Michele/DoorInteractable.cs
@@ -6,7 +6,10 @@ public class DoorInteractable : Interactable
 {
     
     [SerializeField] AudioSource suonoPorta;
+    [SerializeField] AudioClip lockedSound = null;
     [SerializeField] Vector3 rotationAxis = Vector3.up;
+    [SerializeField] bool locked = false;
+    AudioClip sp;
     private bool open = false;
     float closedRotation;
     float openRotation;
@@ -16,6 +19,11 @@ public class DoorInteractable : Interactable
     float openingTime = 0.5f;
 
     void Start(){
+        if (TryGetComponent<AudioSource>(out AudioSource audioSource) && locked) //Controlla se ha l'audio :)
+        {
+            sp = suonoPorta.clip;
+        }
+        
         closedRotation = transform.localEulerAngles.z;
         if (inverted){
             rot = -rot;
@@ -34,24 +42,42 @@ public class DoorInteractable : Interactable
             transform.localEulerAngles = Vector3.Lerp(rotationAxis.normalized * closedRotation, rotationAxis.normalized * openRotation, percent);
     }
 
-
-
     // Does something, called when "interact" is pressed
     public override void Interact(PlayerInteract player){
-        open = !open;
-        if (TryGetComponent<AudioSource>(out AudioSource suonoPorta)) //Controlla se ha l'audio :)
-        {
-            suonoPorta.Play(); //suono apertura porta
-        }
-        
-        if (open)
-        {
-            elapsedTime = 0;
-        }
-        else
-        {
-            elapsedTime = openingTime;        
-        }  
         base.Interact(player);
+        if (TryGetComponent<AudioSource>(out AudioSource audioSource) && locked) //Controlla se ha l'audio :)
+        {
+                audioSource.clip = lockedSound;
+                audioSource.Play(); //suono porta lockata
+        }
+        if (locked && !player.getHasKey()){
+            List<DialogueObject> dialogues = new List<DialogueObject>();
+            DialogueObject d = new DialogueObject();
+            d.text = "Locked";
+            d.timeToNextLine = 2;
+            dialogues.Add(d);
+            UI_Controller.instance.AddDialogues(dialogues.ToArray());
+        }
+        else if (locked && player.getHasKey()){
+            
+            locked = false;
+            player.setHasKey(false);
+        }
+        if (!locked){
+            if (TryGetComponent<AudioSource>(out AudioSource audioSource1)) //Controlla se ha l'audio :)
+            {
+                audioSource1.clip = sp;
+                audioSource1.Play(); //suono apertura porta
+            }
+            open = !open;
+            if (open)
+            {
+                elapsedTime = 0;
+            }
+            else
+            {
+                elapsedTime = openingTime;        
+            }  
+        }
     }
 }
